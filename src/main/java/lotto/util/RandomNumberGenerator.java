bd9350fcf1c762b8879f3120cf5a4042b970a44c
sunmok <tjsahr9191@naver.com>
2024-09-19 17:46:12 +0900

    refactor : LottoConfig 상수를 이용해서 기존 값 매직스트링으으로 변경

--- src/main/java/lotto/util/RandomNumberGenerator.java
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -1,13 +1,18 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.config.LottoConfig;
 
 import java.util.List;
 
 public class RandomNumberGenerator {
 
+    private final static int startNumber = LottoConfig.RANDOM_START_NUMBER;
+    private final static int endNumber = LottoConfig.RANDOM_END_NUMBER;
+    private final static int lottoCount = LottoConfig.LOTTO_NUMBER_COUNT;
+
     public static List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(startNumber, endNumber, lottoCount);
     }
 
 }
