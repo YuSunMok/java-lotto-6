17a2a81ac37edbb999201fc7c692de4f7f9de7f7
sunmok <tjsahr9191@naver.com>
2024-09-21 15:59:08 +0900

    feat : 랜덤 입력 정렬 기능 추가

--- src/main/java/lotto/util/RandomNumberGenerator.java
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -1,18 +1,29 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import lotto.config.LottoConfig;
+import lotto.config.LottoGameConfig;
 
+import java.util.Collections;
 import java.util.List;
 
 public class RandomNumberGenerator {
 
-    private final static int startNumber = LottoConfig.RANDOM_START_NUMBER;
-    private final static int endNumber = LottoConfig.RANDOM_END_NUMBER;
-    private final static int lottoCount = LottoConfig.LOTTO_NUMBER_COUNT;
+    private final static int startNumber = LottoGameConfig.RANDOM_START_NUMBER;
+    private final static int endNumber = LottoGameConfig.RANDOM_END_NUMBER;
+    private final static int lottoCount = LottoGameConfig.LOTTO_NUMBER_COUNT;
 
-    public static List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(startNumber, endNumber, lottoCount);
+    public static List<Integer> generateSorted() {
+        List<Integer> randomNumbers = getRandomNumbers();
+        Collections.sort(randomNumbers);
+        return randomNumbers;
+    }
+
+    private static List<Integer> getRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(
+                startNumber,
+                endNumber,
+                lottoCount
+        );
     }
 
 }
