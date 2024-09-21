622a5809a021393b5c4e7533fcbaf8137030aa7a
sunmok <tjsahr9191@naver.com>
2024-09-21 16:00:12 +0900

    test : Money 유효성 검사 로직 및 구매 가능한 로또 개수 반환하는 메서드 테스트

---
+++ src/test/java/lotto/Model/MoneyTest.java
@@ -0,0 +1,42 @@
+package lotto.Model;
+
+import lotto.exception.LottoGameException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MoneyTest {
+
+    @Test
+    @DisplayName("Money생성시 0원을 넘어야 한다.")
+    public void createMoney_moneyException() {
+        int money = -100;
+
+        assertThatThrownBy(() -> Money.of(money))
+                .isInstanceOf(LottoGameException.class);
+    }
+
+    @Test
+    @DisplayName("Money생성시 1000원 단위여야 한다.")
+    public void createMoney_unitException() {
+        int money = 100;
+
+        assertThatThrownBy(() -> Money.of(money))
+                .isInstanceOf(LottoGameException.class);
+    }
+
+    @Test
+    void getAvailableLottoCount() {
+        Money money = createMoney(1000);
+
+        int availableLottoCount = money.getAvailableLottoCount();
+
+        assertThat(availableLottoCount).isEqualTo(1);
+    }
+
+    private static Money createMoney(int money) {
+        return Money.of(money);
+    }
+}
