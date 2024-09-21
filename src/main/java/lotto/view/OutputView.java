0f2104eb1edfe265eef55c7e223e3d4d6280a2ff
sunmok <tjsahr9191@naver.com>
2024-09-21 15:58:47 +0900

    feat : 구입을 위한 출력 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,15 @@
 package lotto.view;
 
+import lotto.Model.Money;
+
 public class OutputView {
 
-    public void showCommentForAmountInput() {
+    public void showCommentForMoney() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
+    public void showBuyLottoComments(Money money) {
+        int count = money.getAvailableLottoCount();
+        System.out.println(count + "개를 구매했습니다.");
+    }
 }
