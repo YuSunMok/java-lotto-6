98961345645d60960d5d0744145fd09008f4859b
sunmok <tjsahr9191@naver.com>
2024-09-21 16:40:19 +0900

    test(toString) : 로또 리스트 출력

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import lotto.Model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +25,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("로또 번호 리스트를 출력한다.")
+    void toString_() {
+        // given
+        Lotto lotto = Lotto.of(List.of(1, 2, 3, 4, 5, 6));
+
+        // when
+        String result = lotto.toString();
+
+        // then
+        assertThat(result).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
+
 }
