69600e1df61a500c8a80a53db00d2b5679f29d0d
sunmok <tjsahr9191@naver.com>
2024-09-21 15:56:24 +0900

    feat : 유저로부터 금액 받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,13 @@
 package lotto.view;
 
+import lotto.Model.Money;
+import lotto.util.InputUtil;
+
 public class InputView {
+
+    public Money getMoneyFromUser() {
+        int money = InputUtil.readInt();
+        return Money.of(money);
+    }
+
 }
